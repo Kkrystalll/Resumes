cf78d582cf54a5a09735bd94698d730abb332332
krystal <krystal93002232@gmail.com>
2022-04-19 15:20:18 +0800

    add markdown

--- app/javascript/controllers/markdown-editor_controller.js
+++ app/javascript/controllers/markdown-editor_controller.js
@@ -1,10 +1,24 @@
 import { Controller } from "stimulus";
 import EasyMDE from "easymde";
+import "easymde/dist/easymde.min.css";
 
 export default class extends Controller {
   static targets = ["output"];
+  initialize() {
+    this.editor = null;
+  }
+
   connect() {
-    new EasyMDE({ element: this.element });
+    this.editor = new EasyMDE({
+      element: this.element,
+      spellChecker: false,
+    });
     // console.log(EasyMDE);
+  }
+
+  disconnect() {
+    this.editor.toTextArea();
+    this.editor.cleanup();
+    this.editor = null;
   }
 }
